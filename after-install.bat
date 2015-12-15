0a9c685189c2cb7ca459b8be6787b986acbaf5f6
sbelyaev <sbelyaev@users.noreply.github.com>
2015-12-15 18:15:42 +0500

    Update after-install.bat

--- after-install.bat
+++ after-install.bat
@@ -1,3 +1,7 @@
 set Path=C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\
 rem powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File C:\devops\bin\testsite.ps1 > C:\devops\testsite.log
-powershell.exe Write-Host "test message" >> C:\devops\testsite.log
+powershell.exe Write-Host "begin message" >> C:\devops\testsite.log
+powershell.exe -ExecutionPolicy Bypass -File C:\devops\bin\testsite.ps1 > C:\devops\testsite.log
+powershell.exe Write-Host "end message" >> C:\devops\testsite.log
+dir C:\devops\testsite.log
+dir C:\devops\bin\testsite.ps1
