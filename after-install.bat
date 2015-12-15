1cf48446150693b3430b718bc5ace61eec4d60b8
sbelyaev <sbelyaev@users.noreply.github.com>
2015-12-15 20:18:55 +0500

    Update after-install.bat

--- after-install.bat
+++ after-install.bat
@@ -4,5 +4,6 @@ powershell.exe Write-Host "Hello from $PSHOME" > C:\devops\testsite.log
 powershell.exe Write-Host "begin message" >> C:\devops\testsite.log
 powershell.exe -ExecutionPolicy Bypass -File C:\devops\bin\testsite.ps1 >> C:\devops\testsite.log
 powershell.exe Write-Host "end message" >> C:\devops\testsite.log
+powershell.exe Write-Host ($PSHOME -replace "SysWOW64", "SysNative") >> C:\devops\testsite.log
 dir C:\devops\testsite.log >> C:\devops\testsite.log
 dir C:\devops\bin\testsite.ps1 >> C:\devops\testsite.log
