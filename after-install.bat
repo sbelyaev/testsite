58d865d2bdf2db460d0f427ce78f78cd6161201c
sbelyaev <sbelyaev@users.noreply.github.com>
2015-12-15 21:20:48 +0500

    Update after-install.bat

--- after-install.bat
+++ after-install.bat
@@ -1,4 +1,4 @@
-powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File C:\devops\bin\testsite.ps1 > C:\devops\var\log\testsite.log
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File C:\devops\bin\testsite.ps1 > C:\devops\var\log\testsite.log
 rem C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe Write-Host "Hello from $PSHOME" > C:\devops\testsite.log
 rem C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe Write-Host "begin message" >> C:\devops\testsite.log
 rem C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File C:\devops\bin\testme.ps1 >> C:\devops\testsite.log
