ec22c95bb506a733c9ac88fac5a9cf5b5e31a8b3
sbelyaev <sbelyaev@users.noreply.github.com>
2015-12-15 21:39:24 +0500

    Update after-install.bat

--- after-install.bat
+++ after-install.bat
@@ -1,6 +1,1 @@
-set Path=C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File C:\devops\bin\testsite.ps1 > C:\devops\var\log\testsite.log
-rem C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe Write-Host "Hello from $PSHOME" > C:\devops\testsite.log
-rem C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe Write-Host "begin message" >> C:\devops\testsite.log
-rem C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File C:\devops\bin\testme.ps1 >> C:\devops\testsite.log
-rem C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe Write-Host "end message" >> C:\devops\testsite.log
+powershell.exe -ExecutionPolicy Bypass -File C:\devops\bin\testsite.ps1 > C:\devops\var\log\testsite.log
